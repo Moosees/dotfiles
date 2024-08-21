3a655e07458779b6b50a2c9303c7dcd14311ebb6
Linus Almgren <linus.almgren@gmail.com>
2024-08-21 16:33:30 +0200

    clean up lspconfig file

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -113,14 +113,6 @@ return {
             },
           },
         },
-        eslint = {
-          enable = false,
-          settings = {
-            -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
-            workingDirectories = { mode = 'auto' },
-          },
-        },
-        marksman = {},
       },
       -- you can do any additional lsp server setup here
       -- return true if you don't want this server to be setup with lspconfig
