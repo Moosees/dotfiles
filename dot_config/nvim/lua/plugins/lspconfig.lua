95ced7a4045d98a6a815e88a8880be255e927fe2
Linus Almgren <linus.almgren@gmail.com>
2024-12-10 01:23:46 +0100

    let lazyvim handle lua ls config

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -21,21 +21,13 @@ return {
           -- prefix = "icons",
         },
         severity_sort = true,
-        signs = {
-          text = {
-            [vim.diagnostic.severity.ERROR] = LazyVim.config.icons.diagnostics.Error,
-            [vim.diagnostic.severity.WARN] = LazyVim.config.icons.diagnostics.Warn,
-            [vim.diagnostic.severity.HINT] = LazyVim.config.icons.diagnostics.Hint,
-            [vim.diagnostic.severity.INFO] = LazyVim.config.icons.diagnostics.Info,
-          },
-        },
       },
       -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
       -- Be aware that you also will need to properly configure your LSP server to
       -- provide the inlay hints.
       inlay_hints = {
         enabled = true,
-        -- exclude = { 'vue' }, -- filetypes for which you don't want to enable inlay hints
+        exclude = { 'vue' }, -- filetypes for which you don't want to enable inlay hints
       },
       -- Enable this to enable the builtin LSP code lenses on Neovim >= 0.10.0
       -- Be aware that you also will need to properly configure your LSP server to
@@ -67,34 +59,8 @@ return {
       servers = {
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --    https://github.com/pmizio/typescript-tools.nvim
-        lua_ls = {
-          -- mason = false, -- set to false if you don't want this server to be installed with mason
-          settings = {
-            Lua = {
-              workspace = {
-                checkThirdParty = false,
-              },
-              codeLens = {
-                enable = true,
-              },
-              completion = {
-                callSnippet = 'Replace',
-              },
-              doc = {
-                privateName = { '^_' },
-              },
-              hint = {
-                enable = true,
-                setType = false,
-                paramType = true,
-                paramName = 'Disable',
-                semicolon = 'Disable',
-                arrayIndex = 'Disable',
-              },
-            },
-          },
-        },
         stylelint_lsp = {
+          -- mason = false, -- set to false if you don't want this server to be installed with mason
           filetypes = { 'css', 'scss', 'less', 'sass' },
           -- root_dir = require('lspconfig').util.root_pattern(
           --   'package.json',
@@ -119,8 +85,6 @@ return {
     'williamboman/mason.nvim',
     opts = {
       ensure_installed = {
-        'stylua',
-        -- 'shfmt',
         'emmet-language-server',
         'stylelint',
         'stylelint_lsp',
