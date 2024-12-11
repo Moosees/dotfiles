7f031ccf88d0382dbe72a0f37affd1bf0944ba81
Linus Almgren <linus.almgren@gmail.com>
2024-12-11 22:12:25 +0100

    remove bufferline space between tabs

--- dot_config/nvim/lua/plugins/bufferline.lua
+++ dot_config/nvim/lua/plugins/bufferline.lua
@@ -5,11 +5,31 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons', 'catppuccin/nvim' },
   keys = function()
     return {
-      { '<leader>bp', '<Cmd>BufferLineTogglePin<CR>', desc = '[b]uffers toggle [p]in' },
-      { '<leader>bP', '<Cmd>BufferLineGroupClose ungrouped<CR>', desc = '[b]uffers delete non-[P]inned' },
-      { '<leader>bo', '<Cmd>BufferLineCloseOthers<CR>', desc = '[b]uffers delete [o]ther' },
-      { '<leader>br', '<Cmd>BufferLineCloseRight<CR>', desc = '[buffers] delete to the [r]ight' },
-      { '<leader>bl', '<Cmd>BufferLineCloseLeft<CR>', desc = '[b]uffers delete to the [l]eft' },
+      {
+        '<leader>bp',
+        '<Cmd>BufferLineTogglePin<CR>',
+        desc = '[b]uffers toggle [p]in',
+      },
+      {
+        '<leader>bP',
+        '<Cmd>BufferLineGroupClose ungrouped<CR>',
+        desc = '[b]uffers delete non-[P]inned',
+      },
+      {
+        '<leader>bo',
+        '<Cmd>BufferLineCloseOthers<CR>',
+        desc = '[b]uffers delete [o]ther',
+      },
+      {
+        '<leader>br',
+        '<Cmd>BufferLineCloseRight<CR>',
+        desc = '[buffers] delete to the [r]ight',
+      },
+      {
+        '<leader>bl',
+        '<Cmd>BufferLineCloseLeft<CR>',
+        desc = '[b]uffers delete to the [l]eft',
+      },
       { '<S-h>', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
       { '<S-l>', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
       { '[b', '<cmd>BufferLineMovePrev<cr>', desc = 'Move [b]uffer prev' },
@@ -19,7 +39,7 @@ return {
   opts = {
     options = {
       mode = 'buffers',
-      themable = false,
+      themable = true,
       always_show_bufferline = true,
       show_buffer_close_icons = false,
       show_close_icon = false,
@@ -29,7 +49,7 @@ return {
           text = ' ',
         },
       },
-      separator_style = { '', '' },
+      -- separator_style = { '', '' },
       highlights = require('catppuccin.groups.integrations.bufferline').get(),
       -- highlights = {
       -- background = {
