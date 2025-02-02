f0651006cc8959c17fa8fb6d35750aa1b5ab47d6
Linus Almgren <linus.almgren@gmail.com>
2025-02-02 19:09:00 +0100

    make prettier require config, update plugins

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -43,7 +43,7 @@ vim.g.trouble_lualine = false
 
 -- Enable the option to require a Prettier config file
 -- If no prettier config file is found, the formatter will not be used
-vim.g.lazyvim_prettier_needs_config = false
+vim.g.lazyvim_prettier_needs_config = true
 
 local opt = vim.opt
 
