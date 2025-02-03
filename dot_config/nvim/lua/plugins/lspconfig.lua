17af84e17be794e5584d2bdf8b7a7b36182b49f6
Linus Almgren <linus.almgren@gmail.com>
2025-02-03 20:24:16 +0100

    disable tailwind suppert and change some lspconfig things

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -27,8 +27,8 @@ return {
       -- Be aware that you also will need to properly configure your LSP server to
       -- provide the inlay hints.
       inlay_hints = {
-        enabled = true,
-        exclude = { 'vue' }, -- filetypes for which you don't want to enable inlay hints
+        enabled = false,
+        -- exclude = { 'vue' }, -- filetypes for which you don't want to enable inlay hints
       },
       -- Enable this to enable the builtin LSP code lenses on Neovim >= 0.10.0
       -- Be aware that you also will need to properly configure your LSP server to
@@ -77,6 +77,19 @@ return {
         --     client.server_capabilities.document_formatting = false
         --   end,
         -- },
+        tsserver = {
+          enabled = false,
+        },
+        ts_ls = {
+          enabled = false,
+        },
+        vtsls = {
+          settings = {
+            vtsls = {
+              autoUseWorkspaceTsdk = false,
+            },
+          },
+        },
       },
       -- you can do any additional lsp server setup here
       -- return true if you don't want this server to be setup with lspconfig
